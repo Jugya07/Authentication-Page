02e7e063635de1c94c835bf1867f196fac7d4d95
Jugya K. Gogoi <crjugya123@gmail.com>
2022-12-08 16:05:16 +0530

    Updated and made the apis

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt =  require("bcrypt");
 const app = express();
+const saltRounds = 10;
+const Profile = require("./models/profiles");
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -8,16 +11,70 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+let user = 0;
+
 app.get('/',(req, res)=>{
     res.render("home");
 })
 
-app.get("/login", function(req, res){
+app.get("/login", (req, res)=>{
     res.render("login");
   });
   
-app.get("/register", function(req, res){
-    res.render("register");
+app.get("/register", (req, res)=>{
+    res.render("register" ,{user:user});
   });
+
+app.post("/register",(req,res)=>{
+    Profile.find({email:req.body.email},(err,found)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(found.length == 0){
+                bcrypt.hash(req.body.password , saltRounds ,(err , hash)=>{
+                    if(err){
+                        console.log(err);
+                    }else{
+                        const profile = new Profile({
+                            username:req.body.username,
+                            email:req.body.email,
+                            password:hash
+                        });
+                        profile.save();
+                        res.render("main");
+                    }
+                })
+               
+            }else{
+                let user = 1;
+                res.render("register" , {user : user});
+            }
+        }
+})
+})
+
+app.get("/logout", (req, res)=>{
+      res.redirect('/');
+  });
+  
+app.post("/login",(req,res)=>{
+    Profile.find({email:req.body.email},(err,found)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(found.length == 0){
+                res.render("login");
+            }else{
+                bcrypt.compare(req.body.password, found[0].password, function(err, result) {
+                    if(result == true){
+                        res.render("main");
+                    }else{
+                        res.render("login");
+                    }
+                });
+            }
+        }
+    })
+});
 
 module.exports = app;
